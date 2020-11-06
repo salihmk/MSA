037130b5980c4a3af023c877f13da3dcd8866e83
salihmk <66492278+salihmk@users.noreply.github.com>
2020-11-06 21:11:54 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("MSA")
+})
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(1300)
+    basic.showLeds(`
+        # . # . .
+        . . . . .
+        # # # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(1300)
+    basic.showLeds(`
+        . . # . #
+        . . . . .
+        . . # # #
+        . . . . .
+        . . . . .
+        `)
 })
